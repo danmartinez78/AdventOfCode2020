e8167feb2806a7b3012a30c93336e7a07093c9c3
Daniel Martinez <danmartinez78@gmail.com>
2020-12-02 23:41:50 -0600

    passed part 1

--- day_3.py
+++ day_3.py
@@ -1,34 +1,14 @@
 input_file = open('input.txt', 'r') 
 lines = input_file.readlines() 
   
-
 # part 1
-valid_passwords = 0
-for line in lines: 
-    # print("{}".format(line.strip()))
-    minimum, maximum = line.split()[0].split('-')
-    letter = line.split()[1][0]
-    password = line.split()[2]
-    # print(minimum, maximum, letter, password)
-    letter_count = 0
-    for c in password:
-        if c == letter:
-            letter_count += 1
-    if letter_count >= int(minimum) and letter_count <= int(maximum):
-        valid_passwords += 1
-print(valid_passwords)
-
-# part 2
-valid_passwords = 0
-for line in lines: 
-    # print("{}".format(line.strip()))
-    index_1, index_2 = line.split()[0].split('-')
-    index_1 = int(index_1) - 1
-    index_2 = int(index_2) - 1
-    letter = line.split()[1][0]
-    password = line.split()[2]
-    if password[index_1] == letter and password[index_2] == letter:
-        pass
-    elif password[index_1] == letter or password[index_2] == letter:
-        valid_passwords += 1
-print(valid_passwords)
+# track pos, increment pos + 3 every line and increment trees if char is a #
+# make sure to wrap x 
+pos = 0
+num_trees = 0
+for line in lines:
+    if line[pos] == '#':
+        num_trees += 1
+    pos = pos + 3
+    pos = pos%(len(line)-1)
+print(num_trees)
