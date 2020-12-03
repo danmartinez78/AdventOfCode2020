cb6027515440b70ae5fe6f317b43aeb5393d78c5
Daniel Martinez <danmartinez78@gmail.com>
2020-12-02 23:55:12 -0600

    finished part 2

--- day_3.py
+++ day_3.py
@@ -12,3 +12,29 @@ for line in lines:
     pos = pos + 3
     pos = pos%(len(line)-1)
 print(num_trees)
+
+# part 2
+pos = 0
+num_trees = 0
+num_lines = 0
+line_array = []
+for line in lines:
+    num_lines += 1 # probably better way to do this
+    line_array.append(line)
+
+dx_values = [1, 3, 5, 7, 1]
+dy_values = [1, 1, 1, 1, 2]
+tree_array = []
+
+for dx, dy in zip(dx_values, dy_values):
+    x = 0
+    num_trees = 0
+    for y in range(0, num_lines, dy):
+        line = line_array[y] 
+        if line[x] == '#':
+            num_trees += 1
+        x += dx
+        x = x%(len(line)-1)
+    tree_array.append(num_trees)
+print(*tree_array)
+print(tree_array[0] * tree_array[1] * tree_array[2] * tree_array[3] * tree_array[4]) # lazy lol
